7e6cc51f15aa4f5f4b4a469652c3277bbc38ad55
Avery2 <justinaverychan@gmail.com>
2022-10-09 19:18:54 -0700

    incomplete: attempt Oct 9, 2022 7:18:42 PM

--- testapi.js
+++ testapi.js
@@ -1,12 +1,21 @@
-import OtterApi from 'otter.ai-api';
+import OtterApi from "/Users/avery.chan/Desktop/otter.ai-api/src/index.js";
 
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
-
-const otterApi = new OtterApi.default({
-  email: process.env['EMAIL'], // otter.ai email
-  password: process.env['PASSWORD'], // otter.ai password
+const otterApi = new OtterApi({
+  email: process.env["EMAIL"], // otter.ai email
+  password: process.env["PASSWORD"], // otter.ai password
 });
 
-await otterApi.init() // Performs login
+await otterApi.init(); // Performs login
+
+const UNIX_time = Date.now();
+
+const numHoursBefore = 96;
+const numMillisecondsBefore = numHoursBefore * 60 * 60 * 1000;
+
+const query = "";
+const begin_date = UNIX_time - numMillisecondsBefore;
+const speeches = (await otterApi.advancedSpeechSearch(query, begin_date)) || [];
+console.log({ "speeches.length": speeches.length });
